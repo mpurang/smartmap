7f9ea56017827ef89d0e3ffb0e7dc0aaa3010946
mpurang <meghnap111@gmail.com>
2018-12-26 16:47:23 +1300

    added search widget

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 require([
-  "esri/Map",
-  "esri/views/SceneView"
-], function(Map, SceneView){
-  var map = new Map({
-    basemap: "streets",
-    ground: "world-elevation"
-  });
-  var view = new SceneView({
-    container: "viewDiv",     // Reference to the scene div created in step 5
-    map: map,                 // Reference to the map object created before the scene
-    scale: 50000000,          // Sets the initial scale to 1:50,000,000
-    center: [-101.17, 21.78]  // Sets the center point of view with lon/lat
-  });
+	"esri/Map",
+	"esri/views/SceneView",
+	"esri/widgets/Search",
+], function(Map, SceneView, Search){
+	
+// Code to create the map and view will go here
+var map = new Map({
+	basemap: "topo",
+	ground: "world-elevation"
+});
+
+var view = new SceneView({
+    scale: 123456789,
+    container: "viewDiv",  // Reference to the DOM node that will contain the view
+    map: map  // References the map object created in step 3
+});
+
+// Create an instance of the Search widget
+var searchWidget = new Search({
+    view: view
+});
+
+// Add the search widget to the top right corner of the view
+view.ui.add(searchWidget, {
+    position: "top-right"
+});
+
 });
